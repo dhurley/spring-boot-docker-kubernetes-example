3207191011a6c839b4ac506420e1215d2ef60b86
Donal Hurley <djhurley1990@gmail.com>
2017-04-23 15:26:02 +0100

    Added bean that reads from config map in kubernetes

--- src/main/java/net/djhurley/Application.java
+++ src/main/java/net/djhurley/Application.java
@@ -2,6 +2,7 @@ package net.djhurley;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @SpringBootApplication
 @RestController
+@EnableScheduling
 public class Application {
     @RequestMapping("/")
     public String home() {
